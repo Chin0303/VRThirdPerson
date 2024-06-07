6ce78c1724a277817f34c79c1672201816810370
HuskyGT <103238785+HuskyGT@users.noreply.github.com>
2024-06-07 12:07:02 -0400

    Delete Main directory

--- Main/PluginInfo.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace VRThirdPerson
-{
-    internal class PluginInfo
-    {
-        public const string GUID = "Husky.VRThirdPerson";
-        public const string Name = "VRThirdPerson";
-        public const string Version = "1.0.0";
-    }
-}
